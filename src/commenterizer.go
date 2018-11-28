9b4929e43cd8a3a218415f64d0337c5871449cab
alex <1628499@johnabbottcollege.net>
2018-11-28 13:32:05 -0500

    Started commenterizer in go

---
+++ src/commenterizer.go
@@ -0,0 +1,20 @@
+package commenterizer
+
+import "fmt"
+import "container/list"
+import "os"
+import "io"
+import "io/ioutil"
+
+func main() {
+	
+}
+
+func readFile() list {
+	
+	inString := list.new()
+
+	inString.PushBack()
+}
+
+func 
