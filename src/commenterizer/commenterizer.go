de350ca36d4bf5db0dba940624cf89705bb2d6e5
alex <1628499@johnabbottcollege.net>
2018-11-30 11:11:50 -0500

    functions for finding signatures

--- src/commenterizer/commenterizer.go
+++ src/commenterizer/commenterizer.go
@@ -7,9 +7,31 @@ import (
 	"strings"
 )
 
+// Globals
+
+// LANGUAGE the language of the code file being commented
 var LANGUAGE string
 
+// PARAMSTR the current string for parameters
+var PARAMSTR string
+
+// LANGMAP maps languages to function keywords
+var LANGMAP map[string]string
+
+// ParseState enum for parsing
+type ParseState int
+
+// Define Parsestate enumerator
+const (
+	Start      ParseState = 0
+	Whitespace ParseState = 1
+	InText     ParseState = 2
+)
+
 func main() {
+
+	LANGMAP["go"] = "func"
+	LANGMAP["bash"] = "function"
 
 	// get arguments excluding program
 	args := os.Args[1:]
@@ -36,9 +58,6 @@ func readFile(filename string) []string {
 		os.Exit(1)
 	}
 
-	// get length of byte array
-	//n := bytes.Index(data, []byte{0})
-
 	// convert byte array to string
 	s := string(data)
 
@@ -48,6 +67,127 @@ func readFile(filename string) []string {
 	return inString
 }
 
-func commentLoop() {
+func commentLoop(codeString []string) string {
 
+	var outString string
+
+	for arr := codeString; len(arr) > 0; arr = arr[1:] {
+		if isFunction(arr[0]) {
+
+		}
+	}
+
+	// return the complete code file
+	// with the comment headers added
+	return outString
 }
+
+func getCommentHeader(funcHeader string, parameters []string) string {
+
+	done := false
+	header := ""
+
+	// while the header is not done
+	// (used if the user made a mistake)
+	for !done {
+
+	}
+
+	return header
+}
+
+func isFunction(in string) bool {
+
+	// get the line as bytes in order
+	// to loop through the characters
+	bytes := []byte(in)
+	correct := true
+
+	// loop through the line characters
+	for i := 0; i < len(bytes); i++ {
+
+		// get the language
+		switch LANGUAGE {
+		case "go":
+
+			// set the function keyword
+			sigStr := LANGMAP["go"]
+			sig := []byte(sigStr)
+
+			// if whitespace, ignore it
+			if bytes[i] == ' ' {
+				continue
+			}
+
+			// compare line start and function keyword
+			for tmp := bytes[i:len(sig)]; len(tmp) > 0; {
+
+				// If it is not a match then it is not a function
+				if tmp[0] != sig[0] {
+					return false
+				}
+
+				// loop through both strings
+				tmp = tmp[1:]
+				sig = sig[1:]
+			}
+		default:
+			println(LANGUAGE + "is not supported")
+			os.Exit(1)
+		}
+	}
+
+	return correct
+}
+
+func getFunctionName(in string) string {
+
+	// get the line as bytes in order
+	// to loop through the characters
+	bytes := []byte(in)
+
+	// loop through the line characters
+	for i := 0; i < len(bytes); i++ {
+		// get the language
+		switch LANGUAGE {
+		case "go":
+
+			// set the function keyword
+			sigStr := LANGMAP["go"]
+			sig := []byte(sigStr)
+
+			// if whitespace, ignore it
+			if bytes[i] == ' ' {
+				continue
+			}
+
+			state := Start
+
+			// compare line start and function keyword
+			for tmp := bytes[i:len(sig)]; len(tmp) > 0; {
+
+				switch state {
+
+				case Start:
+					// If it is not a match then it is not a function
+					if tmp[0] != sig[0] {
+
+					}
+				case Whitespace:
+				}
+
+				// loop through both strings
+				tmp = tmp[1:]
+				sig = sig[1:]
+			}
+		default:
+			println(LANGUAGE + "is not supported")
+			os.Exit(1)
+		}
+	}
+	return " "
+}
+
+/*func getFunctionParameters(in string) []string {
+
+}*/
